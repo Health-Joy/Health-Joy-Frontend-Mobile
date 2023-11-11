2791a80692e0dfc1c6eadc7f7843275239ea5d04
fatiihvarol <fatihvrl38@gmail.com>
2023-11-12 00:13:14 +0300

    Popup and image picker configration

--- Health_Joy_Frontend_Mobile/Components/Navbar.js
+++ Health_Joy_Frontend_Mobile/Components/Navbar.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   TouchableOpacity,
   Image,
   StyleSheet,
   Dimensions,
+  Modal,
+  Text,
+  TouchableWithoutFeedback,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 
@@ -12,44 +15,75 @@ const windowWidth = Dimensions.get('window').width;
 
 const Navbar = () => {
   const navigation = useNavigation();
+  const [isMenuVisible, setMenuVisible] = useState(false);
 
-  const handleImageClick = () => {};
+  const navigateToScreen = screenName => {
+    navigation.navigate(screenName);
+    setMenuVisible(false); // Close the menu after navigating
+  };
+  const handleCamera = () => {};
+  const handleLibrary = () => {};
 
   return (
     <View style={styles.navbar}>
       <View style={styles.imageContainer}>
-        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <TouchableOpacity onPress={() => navigateToScreen('Home')}>
           <Image
             source={require('../assets/navbar-icons/home-icon.png')}
             style={styles.image}
           />
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Search')}>
+        <TouchableOpacity onPress={() => navigateToScreen('Search')}>
           <Image
             source={require('../assets/navbar-icons/search-icon.png')}
             style={styles.image}
           />
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('TextRecognitionn')}>
+
+        {/* Barcode Icon with Modal */}
+        <TouchableOpacity onPress={() => setMenuVisible(true)}>
           <Image
             source={require('../assets/navbar-icons/barcode-icon.png')}
             style={styles.image}
           />
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleImageClick}>
+
+        <TouchableOpacity onPress={() => navigateToScreen('Favorite')}>
           <Image
             source={require('../assets/navbar-icons/favorite-icon.png')}
             style={styles.image}
           />
         </TouchableOpacity>
-        <TouchableOpacity onPress={handleImageClick}>
+        <TouchableOpacity onPress={() => navigateToScreen('Profile')}>
           <Image
             source={require('../assets/navbar-icons/profile-icon.png')}
             style={styles.image}
           />
         </TouchableOpacity>
       </View>
+
+      <Modal
+        visible={isMenuVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={() => setMenuVisible(false)}>
+        <TouchableWithoutFeedback onPress={() => setMenuVisible(false)}>
+          <View style={styles.menuOverlay}>
+            <View style={styles.menu}>
+              <TouchableOpacity onPress={() => handleLibrary}>
+                <Text style={styles.menuItem}>Choose From Library </Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => handleCamera}>
+                <Text style={styles.menuItem}>Take Photo</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setMenuVisible(false)}>
+                <Text style={styles.menuItem}>Cancel</Text>
+              </TouchableOpacity>
+              {/* Add more menu options as needed */}
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </Modal>
     </View>
   );
 };
@@ -71,6 +105,20 @@ const styles = StyleSheet.create({
   image: {
     width: 35,
     height: 35,
+  },
+  menuOverlay: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'flex-end',
+  },
+  menu: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+  },
+  menuItem: {
+    fontSize: 18,
+    paddingVertical: 10,
   },
 });
 
